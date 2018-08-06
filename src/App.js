851f8d0950e9955276da383c56b313ebf9f0de39
Sean Bhupathi <seanbhupathi@gmail.com>
2018-08-06 17:36:10 -0400

    domain name fixed to exclude herokuapp also jinyang in the building

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './Pacman.svg';
+import jinyang from './jinyang.png'
 import './App.css';
 import Typist from 'react-typist';
 
@@ -8,16 +9,16 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo"/>
 
 
         </header>
         <Typist className="App-title">
             <span>I love you</span>
-            <Typist.Backspace count={10} delay={3000} />
+            <Typist.Backspace count={10} delay={3000}/>
             <span>Under Construction</span>
         </Typist>
-
+        <img src={jinyang} className="jinyang"/>
       </div>
     );
   }
